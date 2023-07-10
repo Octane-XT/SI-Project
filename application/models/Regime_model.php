e9d0c1471291d4fceff5304af2a12cc2eb0b3da5
Ambinintsoa <randriambololonaisabelle@gmail.com>
2023-07-10 18:48:20 +0300

    dashboard

--- application/models/Regime_model.php
+++ application/models/Regime_model.php
@@ -3,4 +3,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Regime_model extends CI_Model
 {
+    public function getAllRegime()
+    {
+        $query = $this->db->get('regime');
+        return $query->result();
+    }
 }
