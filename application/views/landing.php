a440584e1741aaed3bd7050528bf3d33645384c8
Octane-XT <zorakotoasimbola@hotmail.com>
2023-07-10 07:03:46 -0700

    Create landing.php

---
+++ application/views/landing.php
@@ -0,0 +1,40 @@
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="<?php echo base_url('assets/css/landing.css'); ?>">
+
+  <section class="hero">
+    <div class="hero__thumb">
+      <img src="https://images.unsplash.com/photo-1681068420510-cb528c65d595?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80" alt="">
+    </div>
+    <div class="hero__caption">
+      <h1>"Adoptez une alimentation équilibrée pour une santé optimale !"</h1>
+      <p>IZAY METY</p>
+    </div>
+  </section>
+  <section class="content flow">
+    <h1>IZAY METY</h1>
+    <p>
+      Sur ce site, nous vous guiderons dans votre parcours vers une alimentation équilibrée, en vous fournissant des conseils pratiques, des recettes saines et des stratégies pour maintenir une relation positive avec la nourriture. Préparez-vous à nourrir votre corps avec soin et à découvrir les bienfaits d'une alimentation équilibrée pour une santé optimale !
+    </p>
+    <h2>Les principes d'une alimentation équilibrée : Nourrir votre corps avec soin</h2>
+    <p>
+      Lorsqu'il s'agit de prendre soin de notre corps, l'alimentation joue un rôle essentiel. Une alimentation équilibrée fournit à notre organisme les nutriments nécessaires pour fonctionner de manière optimale, renforce notre système immunitaire, favorise la perte de poids et maintient notre énergie tout au long de la journée.
+    </p>
+    <p>
+      Mais quels sont les principes clés d'une alimentation équilibrée ? Tout d'abord, il est important de consommer une variété d'aliments provenant de toutes les catégories alimentaires. Les légumes et les fruits fournissent des vitamines, des minéraux et des fibres essentiels, tandis que les céréales complètes comme le riz brun, les pâtes complètes et le pain complet apportent des glucides complexes et des fibres.
+    </p>
+    <p>
+      Les protéines maigres, telles que le poulet, la dinde, le poisson, les légumineuses et les produits laitiers faibles en gras, sont essentielles pour la croissance et la réparation des tissus, ainsi que pour la santé musculaire. N'oublions pas les bonnes sources de matières grasses, comme les avocats, les noix et les huiles saines, qui fournissent des acides gras essentiels pour le fonctionnement du cerveau et la santé du cœur.
+    </p>
+    <p>
+      Un autre principe important est de limiter la consommation d'aliments transformés, riches en sucre ajouté, en gras saturés et en sel. Les aliments transformés, tels que les boissons sucrées, les snacks pré-emballés et les plats préparés, sont souvent pauvres en nutriments et peuvent contribuer à une prise de poids excessive et à des problèmes de santé.
+    </p>
+    <p>
+      En plus de manger les bons aliments, il est crucial de contrôler les portions et d'écouter les signaux de faim et de satiété de notre corps. Manger lentement, savourer chaque bouchée et être conscient de nos sensations alimentaires nous permet de mieux réguler notre appétit et de ne pas manger plus que nécessaire.
+    </p>
+    <p>
+      En résumé, adopter une alimentation équilibrée consiste à choisir des aliments variés et nutritifs, à limiter les aliments transformés et à contrôler les portions. Cela nécessite une planification des repas, des achats intelligents et une attention portée à nos choix alimentaires au quotidien.
+    </p>
+    </section>
