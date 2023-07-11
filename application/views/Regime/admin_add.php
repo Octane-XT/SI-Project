d1355442f0e94d5d99ed895d323ab17333734b30
NyKanto-crypto <nykantorandri@gmail.com>
2023-07-11 05:47:41 +0300

    parsley

--- application/views/Regime/admin_add.php
+++ application/views/Regime/admin_add.php
@@ -2,18 +2,18 @@
     <div class="card-body">
         <div class="card-title">Ajoutez Regime</div>
         <hr>
-        <form method="POST" action="<?php echo base_url('Regime/add'); ?>">
+        <form method="POST" action="<?php echo base_url('Regime/add'); ?>" id="mon-formulaire">
             <div class="form-group">
                 <label for="input-1">Nom</label>
                 <div style="width: 300px;">
-                    <input type="text" name="nom" class="form-control" id="input-1" placeholder="Entrer le nom de l'aliment">
+                    <input type="text" name="nom" class="form-control" id="input-1" placeholder="Entrer le nom de l'aliment" data-parsley-required="true">
                 </div>
             </div>
 
             <div class="form-group">
                 <label for="input-1">Type Regime</label>
                 <div style="width: 300px;">
-                    <select name="type" class="form-control" id="">
+                    <select name="type" class="form-control" data-parsley-required="true">
                         <option value="">Choisissez Type</option>
                         <option value="1">Augmentation du poids</option>
                         <option value="-1">Diminution du poids</option>
@@ -25,27 +25,28 @@
                 <label>Petit dejeuner</label>
                 <?php for ($i = 0; $i < count($listPetitDejeuner); $i++) { ?>
                     <div class="form-check" style="width: 30%;">
-                        <input class="form-check-input" type="checkbox" name="petitDejeuner[]" value="<?php echo $listPetitDejeuner[$i]['id']; ?>">
+                        <input class="form-check-input" type="checkbox" name="petitDejeuner[]" id="checkbox1-<?php echo $i; ?>" value="<?php echo $listPetitDejeuner[$i]['id']; ?>">
                         <label class="form-check-label"><?php echo $listPetitDejeuner[$i]['nom']; ?></label>
                         <div class="input-group">
-                            <input type="number" name="quantitepetitdej[]" class="form-control" placeholder="Quantité" style="width: 70px;">
-                            <input type="number" name="prixpetitdej[]" class="form-control" placeholder="Prix" style="width: 70px;">
-                            <input type="number" name="poidspetitdej[]" class="form-control" placeholder="Poids" style="width: 70px;">
+                            <input type="number" name="quantitepetitdej[]" class="form-control" placeholder="Quantité" style="width: 70px;" data-parsley-required-if="#checkbox1-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="prixpetitdej[]" class="form-control" placeholder="Prix" style="width: 70px;" data-parsley-required-if="#checkbox1-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="poidspetitdej[]" class="form-control" placeholder="Poids" style="width: 70px;" data-parsley-required-if="#checkbox1-<?php echo $i; ?>:checked" min="0">
                         </div>
                     </div>
                 <?php } ?>
             </div>
 
+
             <div class="form-group">
                 <label>Gouter</label>
                 <?php for ($i = 0; $i < count($listGouter); $i++) { ?>
                     <div class="form-check" style="width: 30%;">
-                        <input class="form-check-input" type="checkbox" name="Gouter[]" value="<?php echo $listGouter[$i]['id']; ?>">
+                        <input class="form-check-input" type="checkbox" name="Gouter[]" id="checkbox2-<?php echo $i; ?>" value="<?php echo $listGouter[$i]['id']; ?>">
                         <label class="form-check-label"><?php echo $listGouter[$i]['nom']; ?></label>
                         <div class="input-group">
-                            <input type="number" name="quantitegouter[]" class="form-control" placeholder="Quantité" style="width: 70px;">
-                            <input type="number" name="prixgouter[]" class="form-control" placeholder="Prix" style="width: 70px;">
-                            <input type="number" name="poidsgouter[]" class="form-control" placeholder="Poids" style="width: 70px;">
+                            <input type="number" name="quantitegouter[]" class="form-control" placeholder="Quantité" style="width: 70px;" data-parsley-required-if="#checkbox2-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="prixgouter[]" class="form-control" placeholder="Prix" style="width: 70px;" data-parsley-required-if="#checkbox2-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="poidsgouter[]" class="form-control" placeholder="Poids" style="width: 70px;" data-parsley-required-if="#checkbox2-<?php echo $i; ?>:checked" min="0">
                         </div>
                     </div>
                 <?php } ?>
@@ -55,12 +56,12 @@
                 <label>Dejeuner</label>
                 <?php for ($i = 0; $i < count($listDejeuner); $i++) { ?>
                     <div class="form-check" style="width: 30%;">
-                        <input class="form-check-input" type="checkbox" name="Dejeuner[]" value="<?php echo $listDejeuner[$i]['id']; ?>">
+                        <input class="form-check-input" type="checkbox" name="Dejeuner[]" id="checkbox3-<?php echo $i; ?>" value="<?php echo $listDejeuner[$i]['id']; ?>">
                         <label class="form-check-label"><?php echo $listDejeuner[$i]['nom']; ?></label>
                         <div class="input-group">
-                            <input type="number" name="quantitedejeuner[]" class="form-control" placeholder="Quantité" style="width: 70px;">
-                            <input type="number" name="prixdejeuner[]" class="form-control" placeholder="Prix" style="width: 70px;">
-                            <input type="number" name="poidsdejeuner[]" class="form-control" placeholder="Poids" style="width: 70px;">
+                            <input type="number" name="quantitedejeuner[]" class="form-control" placeholder="Quantité" style="width: 70px;" data-parsley-required-if="#checkbox3-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="prixdejeuner[]" class="form-control" placeholder="Prix" style="width: 70px;" data-parsley-required-if="#checkbox3-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="poidsdejeuner[]" class="form-control" placeholder="Poids" style="width: 70px;" data-parsley-required-if="#checkbox3-<?php echo $i; ?>:checked" min="0">
                         </div>
                     </div>
                 <?php } ?>
@@ -70,12 +71,12 @@
                 <label>Diner</label>
                 <?php for ($i = 0; $i < count($listDiner); $i++) { ?>
                     <div class="form-check" style="width: 30%;">
-                        <input class="form-check-input" type="checkbox" name="Diner[]" value="<?php echo $listDiner[$i]['id']; ?>">
+                        <input class="form-check-input" type="checkbox" name="Diner[]" id="checkbox4-<?php echo $i; ?>" value="<?php echo $listDiner[$i]['id']; ?>">
                         <label class="form-check-label"><?php echo $listDiner[$i]['nom']; ?></label>
                         <div class="input-group">
-                            <input type="number" name="quantitediner[]" class="form-control" placeholder="Quantité" style="width: 70px;">
-                            <input type="number" name="prixdiner[]" class="form-control" placeholder="Prix" style="width: 70px;">
-                            <input type="number" name="poidsdiner[]" class="form-control" placeholder="Poids" style="width: 70px;">
+                            <input type="number" name="quantitediner[]" class="form-control" placeholder="Quantité" style="width: 70px;" data-parsley-required-if="#checkbox4-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="prixdiner[]" class="form-control" placeholder="Prix" style="width: 70px;" data-parsley-required-if="#checkbox4-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="poidsdiner[]" class="form-control" placeholder="Poids" style="width: 70px;" data-parsley-required-if="#checkbox4-<?php echo $i; ?>:checked" min="0">
                         </div>
                     </div>
                 <?php } ?>
@@ -85,12 +86,12 @@
                 <label>Activites Sportives</label>
                 <?php for ($i = 0; $i < count($listSport); $i++) { ?>
                     <div class="form-check" style="width: 30%;">
-                        <input class="form-check-input" type="checkbox" name="sport[]" value="<?php echo $listSport[$i]->id; ?>">
+                        <input class="form-check-input" type="checkbox" name="sport[]" id="checkbox5-<?php echo $i; ?>" value="<?php echo $listSport[$i]->id; ?>">
                         <label class="form-check-label"><?php echo $listSport[$i]->nom; ?></label>
                         <div class="input-group">
-                            <input type="number" name="frequencesport[]" class="form-control" placeholder="Frequence" style="width: 70px;">
-                            <input type="number" name="prixsport[]" class="form-control" placeholder="Prix" style="width: 70px;">
-                            <input type="number" name="poidssport[]" class="form-control" placeholder="Poids" style="width: 70px;">
+                            <input type="number" name="frequencesport[]" class="form-control" placeholder="Frequence" style="width: 70px;" data-parsley-required-if="#checkbox5-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="prixsport[]" class="form-control" placeholder="Prix" style="width: 70px;" data-parsley-required-if="#checkbox5-<?php echo $i; ?>:checked" min="0">
+                            <input type="number" name="poidssport[]" class="form-control" placeholder="Poids" style="width: 70px;" data-parsley-required-if="#checkbox5-<?php echo $i; ?>:checked" min="0">
                         </div>
                     </div>
                 <?php } ?>
@@ -102,3 +103,9 @@
         </form>
     </div>
 </div>
+<script>
+    $(document).ready(function() {
+        $('#mon-formulaire').parsley();
+    });
+</script>
+<script src="<?php echo base_url('assets/js/parsley.min.js'); ?>"></script>
